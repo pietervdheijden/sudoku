a6f53779314add606c3e784da93375313e9be67a
Pieter van der Heijden <pietervdheijden@gmail.com>
2022-08-16 20:44:35 +0200

    Added method "XY wing"

--- main.go
+++ main.go
@@ -10,23 +10,35 @@ import (
 )
 
 func main() {
-    // solve("sudoku_level1.txt")
-	// solve("sudoku_level6_1.txt") // edition 225, puzzle 75
-	// solve("sudoku_level6_2.txt") // edition 225, puzzle 77
-	// solve("sudoku_level6_3.txt") // edition 225, puzzle 79
-	// solve("sudoku_level6_4.txt") // edition 225, puzzle 83
-	// solve("sudoku_level8_1.txt") // edition 134, puzzle 26
-	// solve("sudoku_level8_2.txt") // edition 134, puzzle 28
-	// solve("sudoku_level8_3.txt") // edition 134, puzzle 30
-	solve("sudoku_level9_1.txt") // edition 134, puzzle 7
-	// solve("sudoku_level9_2.txt") // edition 134, puzzle 17
-	// solve("sudoku_level9_3.txt") // edition 134, puzzle 25
-	// solve("sudoku_level9_4.txt") // edition 134, puzzle 27
-	// solve("sudoku_level9_xwing.txt") // edition 134, techniques, x-wing
-	// solve("sudoku_level9_ywing.txt") // edition 134, techniques, y-wing
+	puzzles := []string{
+		// "sudoku_level1.txt",
+		// "sudoku_level6_1.txt", // edition 225, puzzle 7
+		// "sudoku_level6_2.txt", // edition 225, puzzle 75
+		// "sudoku_level6_3.txt", // edition 225, puzzle 77
+		// "sudoku_level6_4.txt", // edition 225, puzzle 79
+		// "sudoku_level6_5.txt", // edition 225, puzzle 83
+		// "sudoku_level8_1.txt", // edition 134, puzzle 26
+		// "sudoku_level8_2.txt", // edition 134, puzzle 28
+		// "sudoku_level8_3.txt", // edition 134, puzzle 30
+		// "sudoku_level9_1.txt", // edition 134, puzzle 17
+		// "sudoku_level9_2.txt", // edition 134, puzzle 25 (contradiction)
+		"sudoku_level9_3.txt", // edition 134, puzzle 27 (XY + XYZ)
+		// "sudoku_level9_xwing.txt", // edition 134, techniques, x-wing
+		// "sudoku_level9_ywing.txt", // edition 134, techniques, y-wing
+	}
+
+
+	for _, puzzle := range puzzles {
+		success := solve(puzzle)
+		if !success {
+			fmt.Printf("Sudoku %v could not be solved...\n", puzzle)
+			break
+		}
+		fmt.Printf("Sudoku %v: solved!\n", puzzle)
+	}
 }
 
-func solve(puzzle string) {
+func solve(puzzle string) bool {
 	sudoku := Sudoku{}
     sudoku.read(puzzle)
 	i := 0
@@ -35,7 +47,9 @@ func solve(puzzle string) {
 		fmt.Printf("######### Iteration %d ##########\n", i)
 		sudoku.print()
 		if !sudoku.valid() {
-			panic("Sudoku is not valid!")
+			// panic("Sudoku is not valid!")
+			fmt.Println("Sudoku is not valid!")
+			return false
 		}
         for {
             // Keep updating options until all "updates" are found
@@ -50,7 +64,9 @@ func solve(puzzle string) {
 		if !updated {
 			sudoku.print()
             sudoku.printOptions()
-			panic("Couldn't find an option to move. Sudoku is too hard for the algorithm!")
+			// panic("Couldn't find an option to move. Sudoku is too hard for the algorithm!")
+			fmt.Println("Couldn't find an option to move. Sudoku is too hard for the algorithm!")
+			return false
 		}
 		if sudoku.done() {
 			break
@@ -60,6 +76,7 @@ func solve(puzzle string) {
 	fmt.Println("######################")
 	fmt.Println("Done! Sudoku is solved!")
 	sudoku.print()
+	return true
 }
 
 // TODO: consider renaming options to candidates
@@ -359,8 +376,76 @@ func (sudoku *Sudoku) updateOptions() bool {
 	}
 
 	// TODO: implement X wing
-	// TODO: implement XY wing
 	// TODO: implement XYZ wing
+
+	// Use method XY wing to eliminate options
+	// Source: https://www.learn-sudoku.com/xy-wing.html
+	for c1 := 0; c1 < len(sudoku); c1++ {
+		if len(sudoku[c1].options) != 2 {
+			// skip if c1 does not exactly have 2 options
+			continue
+		}
+		for c2 := 0; c2 < len(sudoku); c2++ {
+			if len(sudoku[c2].options) != 2 {
+				// skip if c2 does not exactly have 2 options
+				continue
+			}
+			if equals(sudoku[c1].options, sudoku[c2].options) {
+				// skip if c2 has exactly the same options as c1
+				continue
+			}
+			if !sudoku.affect(c1, c2) {
+				// skip if c1 and c2 do not affect each other
+				continue
+			} 
+			intersectC2 := intersect(sudoku[c2].options, sudoku[c1].options)
+			exceptC2 := except(sudoku[c2].options, sudoku[c1].options)
+			if len(intersectC2) != 1 || len(exceptC2) != 1 {
+				continue
+			}
+			for c3 := 0; c3 < len(sudoku); c3++ {
+				if len(sudoku[c3].options) != 2 {
+					// skip if c3 does not exactly have 2 options
+					continue
+				}
+				if equals(sudoku[c1].options, sudoku[c3].options) {
+					// skip if c3 has exactly the same options as c1
+					continue
+				}
+				if !sudoku.affect(c1, c3) {
+					// skip if c1 and c3 do not affect eachother
+					continue
+				} 
+				intersectC3 := intersect(sudoku[c3].options, sudoku[c1].options)
+				exceptC3 := except(sudoku[c3].options, sudoku[c1].options)
+				if len(intersectC3) != 1 || len(exceptC3) != 1 {
+					continue
+				}
+				if intersectC2[0] == intersectC3[0] || exceptC2[0] != exceptC3[0] {
+					continue
+				}
+
+				// XY wing found! Remove exceptC2 (=exceptC3) from cells affected by c2 and c3.
+				for c4 := 0; c4 < len(sudoku); c4++ {
+					if c4 == c1 || c4 == c2 || c4 == c3 {
+						continue
+					}
+					if sudoku.affect(c2, c4) && sudoku.affect(c3, c4) {
+						removed := sudoku[c4].removeOption(exceptC2[0])
+						if removed {
+							updated = true
+						}	
+					}
+				}
+
+				// sudoku.print()
+				// sudoku.printOptions()
+				// fmt.Printf("XY wing found! c1=%v, c2=%v, c3=%v, c4s=%v\n", c1,c2,c3,c4s)
+				// fmt.Printf("intersectC2=%v, intersectC3=%v, exceptC2=%v, exceptC3=%v\n", intersectC2,intersectC3,exceptC2, exceptC3)
+				// // panic("END")
+			}
+		}
+	}
 
     return updated
 }
@@ -517,6 +602,33 @@ func contains(s []int, e int) bool {
         }
     }
     return false
+}
+
+// returns elements both in s1 and s2
+func intersect(s1 []int, s2 []int) []int {
+	s3 := []int{}
+	for _, v := range s1 {
+		if contains(s2, v) {
+			s3 = append(s3, v)
+		}
+	}
+	return s3
+}
+
+// returns element in s1, but not in s2
+func except(s1 []int, s2 []int) []int {
+	s3 := []int{}
+	for _, v := range s1 {
+		if !contains(s2, v) {
+			s3 = append(s3, v)
+		}
+	}
+	return s3
+}
+
+// returns if cell c1 and c2 affect each other
+func (sudoku *Sudoku) affect(c1 int, c2 int) bool {
+	return sudoku[c1].row == sudoku[c2].row || sudoku[c1].column == sudoku[c2].column || sudoku[c1].square == sudoku[c2].square
 }
 
 func (cell *Cell) removeOption(value int) bool {
