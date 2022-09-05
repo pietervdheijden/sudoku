c77eb0991895987a34f1afa4aa7560d00cb95503
Pieter van der Heijden <pietervdheijden@gmail.com>
2022-09-05 16:51:42 +0200

    Added boilerplate code for frontend

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+
+    <br/>
+
+    <table class="center">
+      <tr v-for="row in 9" :key="row">
+        <td v-for="column in 9" :key="column">
+          {{ row }} {{ column }}
+        </td>
+      </tr>
+    </table>
+
+    <span v-for="n in 10" :key="n">{{ n }}</span>
+
+    <p>Buttons:</p>
+
+    <button>Solve</button>
+    <button>Solve next cell</button>
+    <button>Hint</button>
+    <button>Check</button>
+    <button>Show options / candidates</button>
+  </main>
+</template>
+
+<style>
+.center {
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
